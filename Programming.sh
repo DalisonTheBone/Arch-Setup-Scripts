773f664df50593bc95bc928ee2d099ae86da7712
DalaisonTheBone <141103320+DalisonTheBone@users.noreply.github.com>
2025-07-02 14:06:09 -0230

    Update Programming.sh

--- Programming.sh
+++ Programming.sh
@@ -5,3 +5,6 @@ sudo pacman -S --noconfirm code fastfetch
 # Install Languages
 sudo pacman -Sy --noconfirm python-pip
 sudo pacman -S --noconfirm nodejs npm
+
+# Install Fonts
+sudo pacman -S --noconfirm ttf-dejavu ttf-liberation noto-fonts
