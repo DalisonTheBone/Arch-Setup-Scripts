1a5155ae7f1642b3049cb32ddc23661265067936
DalaisonTheBone <141103320+DalisonTheBone@users.noreply.github.com>
2025-07-02 15:46:13 -0230

    Update SocialApps.sh

--- SocialApps.sh
+++ SocialApps.sh
@@ -8,6 +8,7 @@ sudo pacman -S --noconfirm lutris steam
 
 # Install Wine
 sudo pacman -S --noconfirm wine wine-mono wine-gecko winetricks
+flatpak install flathub net.davidotek.pupgui2 -y
 
 # Install Roblox
 flatpak install -y flathub org.vinegarhq.Sober
