e254b00bc27111ff218238699892ab59ef4866c7
DalaisonTheBone <141103320+DalisonTheBone@users.noreply.github.com>
2025-07-03 14:06:58 -0230

    Update MainApps.sh

--- MainApps.sh
+++ MainApps.sh
@@ -4,4 +4,4 @@
 flatpak install flathub com.brave.Browser
 
 # Install File Manager and Terminal Emulator
-sudo pacman -S --noconfirm dolphin konsole kate
+sudo pacman -S --noconfirm dolphin kitty kate
